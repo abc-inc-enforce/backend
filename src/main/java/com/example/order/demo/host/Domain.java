55d1330725b44cf014fff37adca82c355fe783b8
hsem4717 <hsem4717@gmail.com>
2023-11-21 12:23:06 +0900

    ADD :: 세팅

---
+++ src/main/java/com/example/order/demo/host/Domain.java
@@ -0,0 +1,15 @@
+package com.example.order.demo.host;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class Domain {
+}
